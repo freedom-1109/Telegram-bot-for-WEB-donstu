dad1d9962e32347b1fae131e597c9c1e98cddca5
Nabokov Nikolay <n.nabokov1109@gmail.com>
2022-11-26 11:55:26 +0300

    ну уже точно main

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.dispatcher.filters import Text
 
 
-# Configure logging
+""" логирование сообщений, не понял как пользоваться))) """
 logging.basicConfig(level=logging.INFO)
 
-# Initialize bot and dispatcher
+"""Initialize bot and dispatcher"""
 bot = Bot(token=TG_API)
 dp = Dispatcher(bot)
 
+
+""" создание клавиатур """
 start_kb = InlineKeyboardMarkup(row_width=1)
 start_kb.add(InlineKeyboardButton(text="Новосибирск", callback_data='getNovosibirskWeather'))\
     .row(InlineKeyboardButton(text="Москва", callback_data='getMoscowWeather'), InlineKeyboardButton(text="Санкт-Петербург", callback_data='getLeningradWeather'))\
@@ -20,7 +22,7 @@ start_kb.add(InlineKeyboardButton(text="Новосибирск", callback_data='
 
 back_to_menu_kb = InlineKeyboardMarkup(row_width=1)
 back_to_menu_kb.add(InlineKeyboardButton(text="Назад в меню", callback_data='backToMenu'))
-
+""" конец создания клавиатур """
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
@@ -37,6 +39,7 @@ async def author(message: types.Message):
     await message.answer(f"Я Николай Набоков @nnabokov, разработчик данного бота", reply_markup=back_to_menu_kb)
 
 
+""" возвращает в главное меню """
 @dp.callback_query_handler(Text('backToMenu'))
 async def backToMenu(callback: types.CallbackQuery):
     await bot.edit_message_text(
@@ -47,9 +50,9 @@ async def backToMenu(callback: types.CallbackQuery):
     )
     await callback.answer()
 
-@dp.callback_query_handler(Text(startswith='get') and Text(endswith='Weather'))
-async def getWeather(callback: types.CallbackQuery):
-    city = callback.data[3:-7]
+
+""" запрвшивает погоду с сайта OpenWeatherMap """
+def getWeather(city):
     data = requests.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&lang=ru&appid=' + OWM_API).json()
 
     data['main']['temp_min'] = int(data['main']['temp_min'] + (0.5 if data['main']['temp_min'] > 0 else -0.5))
@@ -57,10 +60,20 @@ async def getWeather(callback: types.CallbackQuery):
     data['wind']['speed'] = int(data['wind']['speed'] + (0.5 if data['wind']['speed'] > 0 else -0.5))
     data['main']['feels_like'] = int(data['main']['feels_like'] + (0.5 if data['main']['feels_like'] > 0 else -0.5))
 
-    temperature = f"от {data['main']['temp_min']} до {data['main']['temp_max']}" if {data['main']['temp_min']} != {data['main']['temp_max']} else f"{data['main']['temp_min']}"
-    message = f"В городе {data['name']} сейчас {data['weather'][0]['description']}, "\
+    temperature = f"от {data['main']['temp_min']} до {data['main']['temp_max']}" \
+        if {data['main']['temp_min']} != {data['main']['temp_max']} else f"{data['main']['temp_min']}"
+    message = f"В городе {data['name']} сейчас {data['weather'][0]['description']}, " \
               f"температура {temperature} по цельсию, " \
               f"но ощущается на {data['main']['feels_like']}. Скорость ветра {data['wind']['speed']} м/с, а облачность {data['clouds']['all']}%."
+
+    return message
+
+
+""" отправляет полученную погоду """
+@dp.callback_query_handler(Text(startswith='get') and Text(endswith='Weather'))
+async def sentWeather(callback: types.CallbackQuery):
+    city = callback.data[3:-7]
+    message = getWeather(city)
 
     await bot.edit_message_text(
         chat_id=callback.message.chat.id,
@@ -70,5 +83,7 @@ async def getWeather(callback: types.CallbackQuery):
         )
     await callback.answer()
 
+
 if __name__ == '__main__':
+    """ запуск бота """
     executor.start_polling(dp, skip_updates=True)
